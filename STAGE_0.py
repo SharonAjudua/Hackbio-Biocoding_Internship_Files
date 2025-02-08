86065ca64797d0b5db822f3bf1d0f19916f7a978
SharonAjudua <sharonajudua@gmail.com>
2025-02-08 19:16:46 +0100

    Add files via upload

---
+++ STAGE_0.py
@@ -0,0 +1,92 @@
+Team_information = [
+{
+"Name" : "Adegite Adejuwon",
+"Slack Username" : "a_adegite",
+"Email" : "adegiteadejuwon@gmail.com",
+"Hobby" : "Teaching",
+"Country" : "Nigeria",
+"Discipline" : "Cell Biology and Genetics",
+"Preferred Programming Language" :"Python"
+},
+{
+"Name" : "Adewuyi Emmanuel",
+"Slack Username" : "EmmanuelEA",
+"Email" : "emmanueladewuyi69@gmail.com",
+"Hobby" : "Photography",
+"Country" : "Nigeria",
+"Discipline" : "Biochemistry",
+"Preferred Programming language" : "Python"
+},
+{
+"Name" : "Mgbafulu Chioma",
+"Slack Username" : "Chioma",
+"Email" : "chiomannamdi23@gmail.com",
+"Hobby" : "Listening to music",
+"Country" : "Nigeria",
+"Discipline" : "Pharmacy",
+"Preferred Programming language" : "Python"
+},
+{
+"Name" : "Ajudua Sharon",
+"Slack Username" : "SharonAjudua",
+"Email" : "sharonajudua@gmail.com",
+"Hobby" : "Trying out new recipes",
+"Country" : "Nigeria",
+"Discipline" : "Medicine",
+"Preferred Programming language" : "Python"
+},
+{
+"Name" : "Maurice Godwin",
+"Slack Username" : "MauriceGodwin",
+"Email" : "mauricemary74@gmail.com",
+"Hobby" : "Reading",
+"Country" : "Nigeria",
+"Discipline" : "Biotechnology",
+"Preferred Programming language" : "Python"
+}]
+
+print("Name:", Team_information[0]["Name"])
+print("Slack Username:", Team_information[0]["Slack Username"])
+print("Email:", Team_information[0]["Email"])
+print("Hobby:", Team_information[0]["Hobby"])
+print("Country:", Team_information[0]["Country"])
+print("Discipline:", Team_information[0]["Discipline"])
+print("Preferred Programming language:", Team_information[0]["Preferred Programming language"])
+print() #Space for team member zero
+
+print("Name:", Team_information[1]["Name"])
+print("Slack Username:", Team_information[1]["Slack Username"])
+print("Email:", Team_information[1]["Email"])
+print("Hobby:", Team_information[1]["Hobby"])
+print("Country:", Team_information[1]["Country"])
+print("Discipline:", Team_information[1]["Discipline"])
+print("Preferred Programming language:", Team_information[1]["Preferred Programming language"])
+print() #Space for team member one
+
+print("Name:", Team_information[2]["Name"])
+print("Slack Username:", Team_information[2]["Slack Username"])
+print("Email:", Team_information[2]["Email"])
+print("Hobby:", Team_information[2]["Hobby"])
+print("Country:", Team_information[2]["Country"])
+print("Discipline:", Team_information[2]["Discipline"])
+print("Preferred Programming language:", Team_information[2]["Preferred Programming language"])
+print() #Space for team member two
+
+print("Name:", Team_information[3]["Name"])
+print("Slack Username:", Team_information[3]["Slack Username"])
+print("Email:", Team_information[3]["Email"])
+print("Hobby:", Team_information[3]["Hobby"])
+print("Country:", Team_information[3]["Country"])
+print("Discipline:", Team_information[3]["Discipline"])
+print("Preferred Programming language:", Team_information[3]["Preferred Programming language"])
+print() #Space for team member three
+
+print("Name:", Team_information[4]["Name"])
+print("Slack Username:", Team_information[4]["Slack Username"])
+print("Email:", Team_information[4]["Email"])
+print("Hobby:", Team_information[4]["Hobby"])
+print("Country:", Team_information[4]["Country"])
+print("Discipline:", Team_information[4]["Discipline"])
+print("Preferred Programming language:", Team_information[4]["Preferred Programming language"])
+
+GITHUB LINKS FOR TEAM MEMBERS
